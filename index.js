d3a9b3408b4cc28f899bcf94cc7467a002d2dfa9
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-04 15:47:00 +0600

    refactoring done

--- index.js
+++ index.js
@@ -24,14 +24,21 @@ const notifyFailure = document.getElementById("notify-failure");
 const actionLeft = document.getElementById("action-left");
 
 let numberOfTryLeft = parseInt(tryNumber.textContent);
-console.log(numberOfTryLeft);
-// adding click event
+
+// adding click event on buttons
 
 pinGeneratorButton.addEventListener("click", () => {
   let randomPassword = Math.floor(1000 + Math.random() * (9999 - 1000 + 1));
   pinGeneratorInput.value = randomPassword;
   actionLeft.style.display = "block";
 });
+
+for (const number of numbers) 
+{
+  number.addEventListener("click", (e) => {
+    calculaorInput.value += e.target.innerText;
+  });
+}
 
 clearButton.addEventListener("click", () => {
   calculaorInput.value = "";
@@ -53,25 +60,32 @@ submitButton.addEventListener("click", () => {
     pinGeneratorInput.value != undefined &&
     pinGeneratorInput.value != "" &&
     pinGeneratorInput.value.length > 0
-  ) {
+  ) 
+  {
     if (areTwoStringEqual(calculaorInput.value, pinGeneratorInput.value)) {
-      notifyFailure.style.display = "none";
-      notifySuccess.style.display = "block";
+
       numberOfTryLeft = 3;
       tryNumber.textContent = numberOfTryLeft;
       pinGeneratorInput.value = "";
       calculaorInput.value = "";
       actionLeft.style.display = "none";
-    } else {
-      console.log("false");
+      notifyFailure.style.display = "none";
+      notifySuccess.style.display = "block";
+
+    } 
+    else 
+    {
       notifyFailure.style.display = "block";
       notifySuccess.style.display = "none";
 
-      if (numberOfTryLeft > 0) {
+      if (numberOfTryLeft > 0) 
+      {
         calculaorInput.value = "";
         numberOfTryLeft = numberOfTryLeft - 1;
         tryNumber.textContent = numberOfTryLeft;
-        if (numberOfTryLeft <= 0) {
+
+        if (numberOfTryLeft <= 0) 
+        {
           submitButton.disabled = true;
           submitButton.style.backgroundColor = "#39458c";
           pinGeneratorInput.value = "";
@@ -81,22 +95,25 @@ submitButton.addEventListener("click", () => {
   }
 });
 
-// function for adding click event
 
-for (const number of numbers) {
-  number.addEventListener("click", (e) => {
-    calculaorInput.value += e.target.innerText;
-  });
-}
 
+/**
+ *this function compares two string that they are equal or not
+ *
+ * @param {string} string1
+ * @param {string} string2
+ * @returns true or false comparing the provided two string
+ */
 function areTwoStringEqual(string1, string2) {
   if (
     string1 != undefined &&
     string1 != "" &&
     string2 != undefined &&
     string2 != ""
-  ) {
-    if (string1 === string2) {
+  )
+   {
+    if (string1 === string2)
+    {
       return true;
     }
   }
